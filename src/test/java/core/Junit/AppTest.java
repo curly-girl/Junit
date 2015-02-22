ec918d8a300f743764d5dcff315ee321974045ac
Ekaterina Presnetsova <kate8607@gmail.com>
2015-02-21 22:12:30 -0800

    JUnit

--- src/test/java/core/Junit/AppTest.java
+++ src/test/java/core/Junit/AppTest.java
@@ -29,65 +29,46 @@ public class AppTest {
 
 	@Test
 	public void test_01_assertEquals_Positive() {
-		System.out.println("Test_01_assertEquals_Positive");
-		String ok="yes";
-		assertEquals("Test fail","yes",ok);
+		assertEquals("String not the same","Testing JUnit 4", App.s);
 	}
 	@Test
 	public void test_02_assertEquals_Negative() {
-		System.out.println("Test_02_assertEquals_Negative");
-		String ok="yes";
-		assertEquals("Test fail","no",ok);
-	}
+		assertEquals("String not the same","Testing TestNG 6", App.s);
+		}
 	@Ignore
 	@Test
 	public void test_03_assertEquals_Ignored() {
-		System.out.println("Test_03_assertEquals_Ignored");
-		String ok="yes";
-		assertEquals("Test fail","yes",ok);
-	}
+		assertEquals("String not the same","Testing JUnit 3", App.s);
+		}
 	@Test
 	public void test_04_assertSame_Positive() {
-		System.out.println("Test_04_assertSame_Positive");
-		Integer i=19;
-		assertSame("Test fail",i,19);
-	}
+		assertSame("Integer not the same",55, App.i);
+		}
+	
 	@Test
 	public void test_05_assertSame_Negative() {
-		System.out.println("Test_05_assertSame_Negative");
-		Integer i=9;
-		assertSame("Test fail",i,19);
-	}
+		assertSame("Integer not the same",54, App.i);
+		}
 	@Ignore
 	@Test
-	public void test_06_assertSame_Positive() {
-		System.out.println("Test_06_assertSame_Positive");
-		Integer i=19;
-		assertSame("Test fail",i,19);
+	public void test_06_assertSame_Ignored() {
+		assertSame("Integer not the same",56,App.i);
 	}
 	@Test
 	public void test_07_assertFalse_Positive() {
-		System.out.println("Test_07_assertFalse_Positive");
-		Boolean f=false;
-		assertFalse("Test fail",f);
+		assertFalse("Boolean should be false",App.f);
 	}
 	@Test
 	public void test_08_assertFalse_Negative() {
-		System.out.println("Test_08_assertFalse_Negative");
-		Boolean f=true;
-		assertFalse("Test fail",f);
+		assertFalse("Boolean should be false",App.t);
 	}
 	@Test
-	public void test_09_assertTrue_Negative() {
-		System.out.println("Test_10_assertTrue_Negative");
-		Boolean t=true;
-		assertFalse("Test fail",t);
+	public void test_09_assertTrue_Positive() {
+		assertTrue("Boolean should be true",App.t);
 	}
 	@Test
 	public void test_10_assertFalse_Positive() {
-		System.out.println("Test_08_assertFalse_Positive");
-		Boolean t=false;
-		assertFalse("Test fail",t);
+		assertTrue("Boolean should be true",App.f);
 	}
 
 }
